435b314f54ab3e4a8a2f748559ea20b5baac715f
Leo Brandoon <leo.gonzalez11@gmail.com>
2020-02-07 13:14:15 -0300

    preimer commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var bienvenido = document.getElementById("form-signin-heading");
+bienvenido.innerHTML = "Por favor iniciar sesión";
+
+var email = document.getElementById("inputEmail");
+email.placeholder = "Correo Electronico";
+
+var pass = document.getElementById("inputPassword");
+pass.placeholder = "contraseña";
+
+var click = document.getElementsByTagName("span")[0];
+click.innerHTML = "Recordar datos";
+
+var login = document.getElementsByTagName("button")[0     
+login.innerHTML = "aprieta aca mi hermano";
